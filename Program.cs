700114bb935e1c6dff6db13ec57811902fec9701
mikekyotn <michael.chankij@mssa.newhorizons.com>
2025-08-06 05:15:41 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Assignment_5._2._1
+{
+    //Given a string s consisting of words and spaces, return the length of the last word
+    //in the string. A word is a maximal substring consisting of non-space characters only.
+    //Status: Complete
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PrintOutput("Fly me to the moon");
+            PrintOutput("It's a beautiful day today");
+
+            Console.ReadKey();
+        }
+        
+        //Find the last space in the sentence, add 1 to that and use in substring for last word
+        static string GetLastWordInString(string sentence)
+        {            
+            int lastSpaceLocation = sentence.LastIndexOf(' ');
+            string lastWord = sentence.Substring(lastSpaceLocation + 1);
+            
+            return lastWord;
+        }
+
+        static void PrintOutput(string input)
+        {
+            string lastWord = GetLastWordInString(input);
+            Console.WriteLine(input);
+            Console.WriteLine($"The last word is '{lastWord}' with length of {lastWord.Length}");
+            Console.WriteLine();
+        }
+    }
+}
